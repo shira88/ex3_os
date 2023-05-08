73deb822113a1838bf71b1ae0a93ca447bf00ba4
shiras <shira.schattner@mail.huji.ac.il>
2023-05-08 20:09:02 +0300

    8.5.23

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -1,11 +1,51 @@
+#include "Barrier.h"
 #include <pthread.h>
-#include <array>
 #include "MapReduceFramework.h"
 
-JobHandle startMapReduceJob(const MapReduceClient& client,
-                            const InputVec& inputVec, OutputVec& outputVec,
-                            int multiThreadLevel)
+struct ThreadContext {
+    std::pair<K1*, V1*> pair;
+    Barrier* barrier;
+    MapReduceClient &client;
+};
+
+void* singleThread(void* arg) // TODO continue
 {
-  auto *thread_array = new pthread_t[multiThreadLevel];
+  ThreadContext* tc = (ThreadContext*) arg;
+
+  // map + sort
+  tc->client.map (tc->pair.first, tc->pair.second, );
+
+  tc->barrier->barrier();
+
+  // reduce
+
+  return 0;
+}
+
+JobHandle startMapReduceJob (const MapReduceClient &client,
+                             const InputVec &inputVec,
+                             OutputVec &outputVec,
+                             int multiThreadLevel)
+{
+  auto threads = new pthread_t[multiThreadLevel];
+  auto contexts = new ThreadContext[multiThreadLevel];
+
+  auto barrier = new Barrier(multiThreadLevel);
+
+  for (int i = 0; i < multiThreadLevel; ++i) {
+    contexts[i] = {inputVec[i], barrier, client};
+  }
+
+  for (int i = 0; i < multiThreadLevel; ++i) {
+    pthread_create(threads + i, NULL, singleThread, contexts + i);
+  }
+
+  for (int i = 0; i < multiThreadLevel; ++i) {
+    pthread_join(threads[i], NULL);
+  }
+
+  delete[] threads;
+  delete[] contexts;
+  delete barrier;
 
 }
